bdb2af036c03a2782e2e474a1a1f9c5daab2b16b
Steven Crutchfield <stevccru@icloud.com>
2021-06-27 16:02:37 -0400

    added more tables

--- employeetrackerschema.sql
+++ employeetrackerschema.sql
@@ -1,7 +1,7 @@
 DROP DATABASE IF EXISTS employeetrackerDB;
 
-
 CREATE DATABASE employeetrackerDB;
+
 
 CREATE TABLE department (
 	id INT PRIMARY KEY,
@@ -14,3 +14,11 @@ CREATE TABLE role (
     salary DECIMAL,
     department_id INT
 );
+
+CREATE TABLE employee (
+	id INT PRIMARY KEY,
+	first_name VARCHAR(30),
+    last_name VARCHAR(30),
+    role_id INT,
+    manager_id INT
+    );
